43c40ce4c1de0042c6e2bf3a50418f96a8492bbf
pfoo <pfoo@unscdf.org>
2026-03-16 09:39:11 +0100

    Ignore cluster local ip

--- pam_notify.sh
+++ pam_notify.sh
@@ -1,19 +1,24 @@
 #! /bin/sh
 
-#This script send an email to the root user when a pam session is opened
-#Add the followings lines at the end of /etc/pam.d/common-session and /etc/pam.d/sudo :
+# This script send an email to the root user when a pam session is opened
+# Add the followings lines at the end of /etc/pam.d/common-session and /etc/pam.d/sudo :
 # session    optional     pam_exec.so /path/to/pam_notify.sh
 
-#Require a mail binary in path
+# Require a mail binary in path
 
 if [ ! "$PAM_TYPE" = "open_session" ]; then
         exit 0
 fi
 
-#proxmox related ? every mortning at 6am
-if [ "$PAM_RUSER" = "root" ] && [ "$PAM_USER" = "nobody" ] && [ "$PAM_RHOST" = "" ] && [ "$PAM_SERVICE" = "su" ]; then
-	exit 0
-fi
+# proxmox related ? every mortning at 6am. Uncomment thoses lines if affected:
+#if [ "$PAM_RUSER" = "root" ] && [ "$PAM_USER" = "nobody" ] && [ "$PAM_RHOST" = "" ] && [ "$PAM_SERVICE" = "su" ]; then
+#	exit 0
+#fi
+
+# don't mail for proxmox cluster IP range (example: 10.10.10.x) as they are used for remote vncproxy sessions. Uncomment thoses lines and edit IP if affected:
+#case "$PAM_RHOST" in
+#  10.10.10.*)  exit 0 ;;
+#esac
 
 {
   echo "User: $PAM_USER"
